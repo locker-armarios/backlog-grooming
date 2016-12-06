e9be817e4508a3fc74cb2d271ade6b491279ee72
Cris Motinha <cristinamotinham@gmail.com>
2016-12-06 08:01:48 -0200

    Divide into School or Company

--- js/backlog-grooming.js
+++ js/backlog-grooming.js
@@ -5,6 +5,8 @@ var sprintApp = angular.module("sprintApp", []);
 sprintApp.controller("reviewController", ["$scope", "$http", function($scope, $http){
 	
 	$scope.topDeliveries = [];
+	$scope.schoolStories = [];
+	$scope.companyStories = [];
 
 
 	//Pegue deste address para mim https://trello.com/b/o9YfxrIj.json
@@ -24,22 +26,38 @@ sprintApp.controller("reviewController", ["$scope", "$http", function($scope, $h
 				return;
 
 			//Se estiver na coluna Top Deliveries
-			if (card.idList == "576ae75d73eebc09e3446f07")
+			if (card.idList == "576ae75d73eebc09e3446f07" || card.idList == "57079ef1b8b39c2443769dc3" 
+				|| card.idList == "57079f06cce9731681c74564" || card.idList == "57c98b47dfbcbfea5e898ef5")
 			{
+				var storyType = findingProject(card.name);
+				if (storyType != 0) {
+				console.log(storyType)
+				if (storyType == 1) {
+					$scope.schoolStories.push({title: card.name}); //tipo escola
+					console.log("sou escola");
+				}
+				else if (storyType == 2) {
+					$scope.companyStories.push({title: card.name}); //tipo empresa
+				}
+				else if (storyType == 3){
+					$scope.schoolStories.push({title: card.name}); //tipo escola E empresa
+					$scope.companyStories.push({title: card.name});
+					console.log("somos amboss");
+				}
+				
 				if (isEpic(card.name) || isTheme(card.name)) {
 					quadro.checklists.forEach(function(checklist){
 						if (card.idChecklists == checklist.id){
 							//pushar os checkitens
 							storiesNestedNames = getStoriesNames(checklist.checkItems);
 							$scope.topDeliveries.push({title: card.name, items: storiesNestedNames});
-							console.log("scopado", $scope.topDeliveries);
-							console.log(storiesNestedNames);
 						}
 					}); 
 				}
 				else {
 					$scope.topDeliveries.push({title: card.name});
 				}
+			}
 				
 			}
 			
@@ -79,4 +97,40 @@ function getStoriesNames(checkitems){
 		storiesNames.push({name: checkitems[i].name});
 	}
 	return storiesNames;
+}
+
+function findingProject(cardname) {
+	var cutCardName = (cardname).substring((cardname).indexOf("[")+1,(cardname).indexOf("]"));
+	isSchool = cutCardName.indexOf("Escola"); //+1
+	isCompany = cutCardName.indexOf("Empresa"); //+2
+	var points = 0;
+
+	if (isSchool != -1) {
+		points+= 1;
+		console.log("escola");
+	}
+	if (isCompany != -1) {
+		points+= 2;
+		console.log("empresa");
+	}
+	return points;
+}
+
+function openStories() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
